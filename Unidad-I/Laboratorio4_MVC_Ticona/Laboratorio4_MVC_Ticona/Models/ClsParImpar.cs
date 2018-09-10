08f3ec5157f64fdf6791ecdf977b10130e3d4386
SERGIO ALEXIS TICONA ARCAYA <serticonaa@upt.pe>
2018-09-09 19:53:06 -0500

    Laboratorio 4 MVC | miércoles, 5 de septiembre de 2018, 20:19

---
+++ Unidad-I/Laboratorio4_MVC_Ticona/Laboratorio4_MVC_Ticona/Models/ClsParImpar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio4_MVC_Ticona.Models
+{
+    public class ClsParImpar
+    {
+        public int Numero { get; set; }
+    }
+}
